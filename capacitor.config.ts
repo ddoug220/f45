8c2cb7b9416680fbc8203b5ce5f7737b14cc749b
Daniel Douglas <danieldouglas@Daniel-MacBook.local>
2024-10-14 12:34:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'f45',
+  webDir: 'www'
+};
+
+export default config;
